df45b75fc435ca798ea558d6a50d1ad1e282b0ce
sanhopmj <sanhopmj@gmail.com>
2021-07-21 18:18:13 +0900

    push  test

--- springProject01/src/main/java/com/oracle/springProject01/controller/Pmj_Controller.java
+++ springProject01/src/main/java/com/oracle/springProject01/controller/Pmj_Controller.java
@@ -1,8 +1,13 @@
 package com.oracle.springProject01.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Pmj_Controller {
-
+	@RequestMapping(value = "test2")
+	public String test() {
+		System.out.println("Pmj_Controller test Start...");
+		return "test2";
+	}
 }
--- springProject01/src/main/webapp/WEB-INF/views/test2.jsp
+++ springProject01/src/main/webapp/WEB-INF/views/test2.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,9 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<%-- <%		response.sendRedirect("test");	%> --%>
-	<%
-		response.sendRedirect("test2");
-	%>
+ push test 0721 18:14
 </body>
 </html>
